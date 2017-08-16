0840621c7ba8252bae07c430a86b13dc73fae499
endor <endor.xmr@gmail.com>
2017-08-17 01:16:17 +0200

    FAQ content rework WIP

--- app/user/help/faq.js
+++ app/user/help/faq.js
@@ -33,11 +33,55 @@ app.controller('FAQCtrl', function($scope, $location, $anchorScroll, $sce, dataS
 
         {
             title: "What is mining and why should I be interested?",
-            answer: $sce.trustAsHtml("Cryptocurrencies achieve decentralisation via a process called mining. When new transactions are created, they need to be validated. Miners compete with each other to validate a group of transactions(a.k.a. block). The winning miner is paid a block reward and collects transaction fees for the work carried out. Block rewards are also how new coins are generated and help regulate the economy of the currency.")
+            answer: $sce.trustAsHtml("Cryptocurrencies achieve decentralisation via a process called mining. When new transactions are created, they need to be validated. Miners compete with each other to validate a group of transactions (a.k.a. block). The winning miner is paid a block reward and collects transaction fees for the work carried out. Block rewards are also how new coins are generated and help regulate the economy of the currency.")
+        },
+        ],
+        "Pool Help": [
+        {
+        	title: "Where can I see my stats (balance, hashrate)?",
+        	answer: $sce.trustAsHtml("You can see your stats in the <a href='#/dashboard'>Dashboard</a> page.")
+        },
+        {
+            title: "How do payouts work?",
+            answer: $sce.trustAsHtml("Every 2 hours a master payment check is executed which pays out all dues. If everything goes as planned all dues that exceed the set payment thresholds are paid out. <br/><br/>In case of a wallet lock up or failure, the pool automatically requeues futher checks at 15 minute intervals until all payments are successfully completed. Once everything is paid out the system returns to the 2 hourly master cycle.<br/><br/> If you have any questions please dont hesitate to contact your pool admin.")
+        },
+        {
+            title: "How do I set my payout threshold?",
+            answer: $sce.trustAsHtml("Payout threshold is the minimum amount that needs to be earned before the pool pays out to your wallet. Since transactions in Monero have miner fees, it's cost effective to set a higher payout threshold for your pool. The minimum value for this is 0.3 XMR.<br/><br/>To change your payment threshold, click the wrench after you login via \"Login\" button on the top right.")
+        },
+        /*{
+            title: "Getting paid in BTC",
+            answer: $sce.trustAsHtml("nodejs-pool supports direct payments to btc. This is done by using the shapeshift API to convert your XMR and send them to a BTC wallet.<br/><br/>To configure BTC payments please have a look at <a href='#/help/getting_started'>Getting Started</a> command line samples.")
+        },*/
+        {
+            title: "Payments to exchanges/markets?",
+            answer: $sce.trustAsHtml("Direct payment to exchange / pool wallets are supported. The only primary difference when using this method is that the default payout threshold is higher (5 XMR).")
+        },
+        {
+            title: "IP Banning?",
+            answer: $sce.trustAsHtml("Your IP gets banned if you submit invalid shares to the pool server. This usually happens if your card is overclocked or unstable.<br/><br/> The ban is temporary and usually cleared in 1-2 minutes. You could also contact your pool admin and request an unban.")
+        },
+        {
+            title: "How Fixed / Variable Difficulty works",
+            answer: $sce.trustAsHtml("When you select a pool port, the starting difficulty only represents your initial setting. As soon as your miner starts submitting shares the server will try to adjust your difficulty to reflect your hash rate.<br/><br/>This assures you not creating too many or too few requests to your server optimizing bandwidth consumption and server loads.<br/><br/>Optionally you could set a fixed difficulty via your miner command line options, though if you set a difficulty too high, you could exceed the 60 seconds job limit and lose earnings.")
+        },
+        /*{
+            title: "Can i mine other coins?",
+            answer: $sce.trustAsHtml("Not yet, but we may add more soon. Follow <a href='https://github.com/Snipa22/nodejs-pool/issues/27' target='_new'>https://github.com/Snipa22/nodejs-pool/issues/27</a>.")
+        }*/
+        ],
+        "Mining":[
+        {
+            title: "What's the best mining hardware?",
+            answer: $sce.trustAsHtml("Monero is an AISC resistant cryptocurrency - that means it should be cost prohibitive to mine monero with an FGPA/AISC - allowing desktop grade hardware to keep its share in the network hashrate and earnings. Both CPUs and GPUs can be used for mining with equally good results.<br/><br/><a href='http://monerobechmarks.info' target='_new'>http://monerobechmarks.info/</a> is a list of community collected hashrate results ordered by hardware, but be careful as some entries may not be accurate.")
+        },
+        {
+            title: "What mining software should I use?",
+            answer: $sce.trustAsHtml("Read the <a href='#/help/getting_started'>Getting Started</a> page for more details.")
         },
         {
             title: "How do I start mining?",
-            answer: $sce.trustAsHtml("You can start mining today if you have a computer that sits idle. Monero can be mined on CPUs, GPU's or even a raspberry PI. To start mining you need to find the right mining software for your hardware and get going.<br/><br/>Read <a hred='#/help/getting_started'>Getting Started</a> for more details.")
+            answer: $sce.trustAsHtml("You can start mining today if you have a computer that sits idle. Monero can be mined on CPUs, GPUs or even a Raspberry PI. To start mining you need to find the right mining software for your hardware and get going.<br/><br/>Read the <a href='#/help/getting_started'>Getting Started</a> page for more details.")
         },
         {
             title: "What is pool mining?",
@@ -45,7 +89,7 @@ app.controller('FAQCtrl', function($scope, $location, $anchorScroll, $sce, dataS
         },
         {
             title: "What is PPLNS?",
-            answer: $sce.trustAsHtml("PPLNS is short for pay per last N shares. It is a method to split miner earnings fairly based on rounds. PPLNS hence favours loyal pool memebers over pool hoppers.")
+            answer: $sce.trustAsHtml("PPLNS is short for Pay Per Last N Shares. It is a method of splitting the block reward fairly among miners. PPLNS favours loyal pool members over pool hoppers. For a more detailed explanation on how PPLNS works, read <a href='http://give-me-coins.com/support/faq/what-is-pplns/'>this page</a>")
         },
         {
             title: "What does SOLO mining mean?",
@@ -53,53 +97,13 @@ app.controller('FAQCtrl', function($scope, $location, $anchorScroll, $sce, dataS
         },
         {
             title: "Is mining profitable?",
-            answer: $sce.trustAsHtml("Mining can be profitable depending on the conditions involved. Your primary cost is your electricity and the cost of your hardware.<br/>It is not practical to calculate the exact amount you would earn as it depends on the total hash rate of the network, difficulty and your luck.<br/><br/>An accurate estimate of earnings of the pool can be calculated by observing average daily number of blocks found ... followed by some mathematics?<br/><br/>* An earnings estimator may be implemented in the future.")
-        },
-        ],
-        "Pool Help": [
-        {
-            title: "How payouts work?",
-            answer: $sce.trustAsHtml("You might often wonder why you haven't been paid yet. This is normal and is how the payment cycle is designed to work.<br/><br/>Every 2 hours a master payment check is executed which pays out all dues. If everything goes as planned all dues that exceed the set payment thresholds are paid out. <br/><br/>In case of a wallet lock up or failure, the pool automatically requeues futher checks at 15 minute intervals until all payments are successfully completed. Once everything is paid out the system returns to the 2 hourly master cycle.<br/><br/> If you have any questions please dont hesitate to contact your pool admin.")
-        },
-        {
-            title: "Payout thresholds?",
-            answer: $sce.trustAsHtml("Payout threshold is the minimum amount that needs to be earned before the pool pays out to your wallet. Since transactions in Monero have a significant miner fees, it's cost effective to set a higher payout threshold for your pool. The minimum value for this is usually 0.3 XMR.<br/><br/>To change your payment threshold, click the wrench after you login via \"Login\" button on the top right.<br/><br/>You could also adjust your payout threshold to regulate your payout schedule etc daily/weekly etc depending on your hash rate.")
-        },
-        {
-            title: "Getting paid in BTC",
-            answer: $sce.trustAsHtml("nodejs-pool supports direct payments to btc. This is done by using the shapeshift API to convert your XMR and send them to a BTC wallet.<br/><br/>To configure BTC payments please have a look at <a href='#/help/getting_started'>Getting Started</a> command line samples.")
-        },
-        {
-            title: "Payments to exchanges/markets?",
-            answer: $sce.trustAsHtml("Direct payment to exchange / pool wallets are supported. The only primary difference when using this method is that the minimum payout threshold is higher and usually a defaults to 3XMR.")
-        },
-        {
-            title: "IP Banning?",
-            answer: $sce.trustAsHtml("Your IP gets banned if you submit invalid shares to the pool server. This usually happens if your card is overclocked or unstable.<br/><br/> The ban is temporary and usually cleared in xx mins. You could also contact your pool admin and request an unban.")
-        },
-        {
-            title: "How Fixed / Variable Difficulty works",
-            answer: $sce.trustAsHtml("When you select a pool port, the starting difficulty only represents your initial setting. As soon as your miner starts submitting shares the server will try to adjust your difficulty to reflect your hash rate.<br/><br/>This assures you not creating too many or too few requests to your server optimizing bandwidth consumption and server loads.<br/><br/>Optionally you could set a fixed difficulty via your miner command line options, though if you set a difficulty too high, you could exceed the 60 seconds job limit and loose earnings.")
-        },
-        {
-            title: "Can i mine other coins?",
-            answer: $sce.trustAsHtml("Not yet, but we may add more soon. Follow <a href='https://github.com/Snipa22/nodejs-pool/issues/27' target='_new'>https://github.com/Snipa22/nodejs-pool/issues/27</a>.")
-        }
-        ],
-        "Mining":[
-        {
-            title: "Hardware?",
-            answer: $sce.trustAsHtml("Monero is an AISC resistant cryptocurrency, that means it should be cost prohibitive to mine monero with an FGPA/AISC allowing desktop grade hardware to keep its share in the network hashrate and earnings.<br/><br/><a href='http://monerobechmarks.byethost5.com/' target='_new'>http://monerobechmarks.byethost5.com/</a> is a list of community collected hashrate results ordered by hardware, but be careful as some entries may not be accurate.")
-        },
-        {
-            title: "Software?",
-            answer: $sce.trustAsHtml("Read -- <a href='#/help/getting_started'>Getting Started</a>.")
+            answer: $sce.trustAsHtml("Mining can be profitable depending on the conditions involved. Your primary cost is your electricity and the cost of your hardware.<br/>It is not practical to calculate the exact amount you would earn as it depends on the total hash rate of the network, difficulty and your luck.<br/><br/>An average estimate of earnings can be calculated by ")
         }
         ],
         "Support":[
         {
             title: "Chat Support",
-            answer: $sce.trustAsHtml("Monero is an AISC resistant cryptocurrency, that means it should be cost prohibitive to mine monero with an FGPA/AISC allowing desktop grade hardware to keep its share in the network hashrate and earnings.<br/><br/><a href='http://monerobechmarks.byethost5.com/' target='_new'>http://monerobechmarks.byethost5.com/</a> is a list of community collected hashrate results ordered by hardware, but be careful as some entries may not be accurate.")
+            answer: $sce.trustAsHtml("If you need more help, drop a message in our <a href='http://supportxmr.chatango.com' target='_new' style='text-decoration: underline'>pool chat</a>.")
         },
         {
             title: "Interesting links.",
