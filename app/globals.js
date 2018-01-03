6c2b4f0ef903600776c31552948da667e330a577
Newbie <linhdqtg@gmail.com>
2018-01-04 03:33:54 +0700

    Update globals.js
    ETN

--- app/globals.js
+++ app/globals.js
@@ -5,7 +5,7 @@ angular.module('pool.globals', [])
 .factory('GLOBALS', function() {
 	return {
 		pool_name: "semiPOOL.com",
-		api_url : 'https://aeon.semipool.com/api',
+		api_url : 'https://etn.semipool.com/api',
 		api_refresh_interval: 30000,
 		app_update_interval: 10*60000
 	};
