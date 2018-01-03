c28d056b60e9bb1e8cfe30b3f506201d8809c338
Newbie <linhdqtg@gmail.com>
2018-01-03 23:31:11 +0700

    Update dashboard.js
    tofix 3

--- app/user/dashboard/dashboard.js
+++ app/user/dashboard/dashboard.js
@@ -4,11 +4,11 @@ app.controller('DashboardCtrl', function($scope , $route, $mdDialog, $pageVisibi
 	$scope.minerStats = {};
 	
         $.getJSON("https://api.cryptonator.com/api/ticker/aeon-usd", function(data) {
-		$scope.aeonusd = (data.ticker.price * 1).toFixed(2);						// CRYPTONATOR XMR/USD RATE
+		$scope.aeonusd = (data.ticker.price * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
 		//$scope.aeonweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.aeonusd;
 	});
 	$.getJSON("https://api.coinmarketcap.com/v1/ticker/electroneum/?convert=USD", function(data) {
-		$scope.etnusd = (data[0].price_usd * 1).toFixed(2);						// CRYPTONATOR XMR/USD RATE
+		$scope.etnusd = (data[0].price_usd * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
 		//$scope.etnweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.etnusd;
 	});	
 	$scope.updateCharts = function (){
