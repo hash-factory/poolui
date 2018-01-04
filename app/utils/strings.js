264f96a72939852a33640aafbae73890b43d846c
Newbie <linhdqtg@gmail.com>
2018-01-04 09:46:29 +0700

    Update strings.js
    Aeon price moved attempt

--- app/utils/strings.js
+++ app/utils/strings.js
@@ -10,6 +10,16 @@ angular.module('utils.strings', [])
 
 .filter('toHashRate', function() {
   return function(hashes) {
+    
+  $.getJSON("https://api.cryptonator.com/api/ticker/aeon-usd", function(data) {
+		$scope.aeonusd = (data.ticker.price * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
+		//$scope.aeonweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.aeonusd;
+	});
+	$.getJSON("https://api.coinmarketcap.com/v1/ticker/electroneum/?convert=USD", function(data) {
+		$scope.etnusd = (data[0].price_usd * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
+		//$scope.etnweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.etnusd;
+	});
+    
     if (hashes > 1000000) {
       return parseFloat((hashes / 1000000).toFixed(2)) + " MH/s"
     }
