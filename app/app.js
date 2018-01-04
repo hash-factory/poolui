64fce34dbb035dd18d0d03f9d2bca5bcacc1a3a0
Newbie <linhdqtg@gmail.com>
2018-01-04 18:37:53 +0700

    Update app.js
    Find new place for 1st price

--- app/app.js
+++ app/app.js
@@ -221,14 +221,6 @@ var app = angular.module('poolui', [
 		timerService.startTimer(GLOBALS.api_refresh_interval);
 		timerService.register(loadData, 'global');
 		$interval(updateCache, GLOBALS.app_update_interval); // check for app updates every 5 mins
-	        	$.getJSON("https://api.cryptonator.com/api/ticker/aeon-usd", function(data) {
-				$scope.aeonusd = (data.ticker.price * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
-				//$scope.aeonweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.aeonusd;
-			});
-			$.getJSON("https://api.coinmarketcap.com/v1/ticker/electroneum/?convert=USD", function(data) {
-				$scope.etnusd = (data[0].price_usd * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
-				//$scope.etnweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.etnusd;
-			});
 		$interval(function(){// Start price timer
 	        	$.getJSON("https://api.cryptonator.com/api/ticker/aeon-usd", function(data) {
 				$scope.aeonusd = (data.ticker.price * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
