1cf7b3fa463a0d29cb371ea8dbbae185018d4a79
Newbie <linhdqtg@gmail.com>
2018-01-04 10:13:56 +0700

    Update app.js
    interval

--- app/app.js
+++ app/app.js
@@ -221,7 +221,7 @@ var app = angular.module('poolui', [
 		timerService.startTimer(GLOBALS.api_refresh_interval);
 		timerService.register(loadData, 'global');
 		$interval(updateCache, GLOBALS.app_update_interval); // check for app updates every 5 mins
-		setInterval(function(){
+		$interval(function(){
                 // Start price timer
 	        	$.getJSON("https://api.cryptonator.com/api/ticker/aeon-usd", function(data) {
 				$scope.aeonusd = (data.ticker.price * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
