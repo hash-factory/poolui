903f1e4f2f6c12a1d20154745e445da258823755
Newbie <linhdqtg@gmail.com>
2018-01-04 10:22:27 +0700

    Update globals.js

--- app/globals.js
+++ app/globals.js
@@ -6,7 +6,7 @@ angular.module('pool.globals', [])
 	return {
 		pool_name: "semiPOOL.com",
 		api_url : 'https://etn.semipool.com/api',
-		api_refresh_interval: 30000,
+		api_refresh_interval: 10000,
 		app_update_interval: 10*60000
 	};
 });
