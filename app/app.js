25f18619d2e39d939960a5614f74b0a44bffa7ad
Newbie <linhdqtg@gmail.com>
2018-01-04 10:19:26 +0700

    Update app.js
    Price update

--- app/app.js
+++ app/app.js
@@ -221,7 +221,16 @@ var app = angular.module('poolui', [
 		timerService.startTimer(GLOBALS.api_refresh_interval);
 		timerService.register(loadData, 'global');
 		$interval(updateCache, GLOBALS.app_update_interval); // check for app updates every 5 mins
-
+		$interval(function(){// Start price timer
+	        	$.getJSON("https://api.cryptonator.com/api/ticker/aeon-usd", function(data) {
+				$scope.aeonusd = (data.ticker.price * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
+				//$scope.aeonweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.aeonusd;
+			});
+			$.getJSON("https://api.coinmarketcap.com/v1/ticker/electroneum/?convert=USD", function(data) {
+				$scope.etnusd = (data[0].price_usd * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
+				//$scope.etnweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.etnusd;
+			});
+		}, 2000); 
 		// Start address tracking servuce after starting timer, only one callback supported at a time
 		addressService.start(function(addrStats) {
 			$scope.addrStats = addrStats;
