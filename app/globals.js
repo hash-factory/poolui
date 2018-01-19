5c4bb7ca17838fceaccc257ed9e583b1e319542f
Newbie <linhdqtg@gmail.com>
2018-01-19 20:46:04 +0700

    revert

--- app/globals.js
+++ app/globals.js
@@ -5,7 +5,7 @@ angular.module('pool.globals', [])
 .factory('GLOBALS', function() {
 	return {
 		pool_name: "semiPOOL.com",
-		api_url : 'https://pool.aeon.semipool.com/api',
+		api_url : 'https://aeon.semipool.com/api',
 		api_refresh_interval: 10000,
 		app_update_interval: 10*60000
 	};
