d1ac024fe267ae145838fb0f211e1665e6fbec89
Newbie <linhdqtg@gmail.com>
2018-01-25 15:31:10 +0700

    Check

--- app/app.js
+++ app/app.js
@@ -210,15 +210,7 @@ var app = angular.module('poolui', [
             				totalLuck += $scope.pulledBlocks[i].shares / $scope.pulledBlocks[i].diff;
             				blockCount += 1;
             		}
-			$scope.overallEffort = (totalLuck / blockCount)*100;
-	        	$.getJSON("https://api.cryptonator.com/api/ticker/aeon-usd", function(data) {
-				$scope.aeonusd = (data.ticker.price * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
-				//$scope.aeonweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.aeonusd;
-			});
-			$.getJSON("https://api.coinmarketcap.com/v1/ticker/electroneum/?convert=USD", function(data) {
-				$scope.etnusd = (data[0].price_usd * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
-				//$scope.etnweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.etnusd;
-			});				
+			$scope.overallEffort = (totalLuck / blockCount)*100;		
 		}
 
 		var loadOnce = function () {
