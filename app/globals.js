3a6ecec2b091145e84e33dd0bbbfe1c5289d6595
Newbie <linhdqtg@gmail.com>
2018-01-27 18:43:35 +0700

    API URL

--- app/globals.js
+++ app/globals.js
@@ -5,7 +5,7 @@ angular.module('pool.globals', [])
 .factory('GLOBALS', function() {
 	return {
 		pool_name: "semiPOOL.com",
-		api_url : 'https://api.aeon.semipool.com',
+		api_url : 'https://aeon-api.semipool.com',
 		api_refresh_interval: 10000,
 		app_update_interval: 10*60000
 	};
