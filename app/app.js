cf6b8bb4360f925ac269c2f42b8e74e749a39919
Learner <linhdqtg@gmail.com>
2018-02-01 18:25:01 +0700

    ticker

--- app/app.js
+++ app/app.js
@@ -252,8 +252,8 @@ var app = angular.module('poolui', [
 		timerService.register(loadData, 'global');
 		$interval(updateCache, GLOBALS.app_update_interval); // check for app updates every 5 mins
 		$interval(function(){ // Start price timer 
-	        	$.getJSON("https://api.cryptonator.com/api/ticker/aeon-usd", function(data) {
-				$scope.aeonusd = (data.ticker.price * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
+	        	$.getJSON("https://api.coinmarketcap.com/v1/ticker/aeon/?convert=USD", function(data) {
+				$scope.aeonusd = (data[0].price_usd * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
 				//$scope.aeonweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.aeonusd;
 			});
 			$.getJSON("https://api.coinmarketcap.com/v1/ticker/electroneum/?convert=USD", function(data) {
