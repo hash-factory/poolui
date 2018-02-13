c2a14c0b4fdfe7d801c362cde4e49d306beeb0c6
Learner <linhdqtg@gmail.com>
2018-02-13 19:08:55 +0700

    Est Price

--- app/user/home/home.js
+++ app/user/home/home.js
@@ -108,10 +108,14 @@ app.controller('HomeCtrl', function($scope, $route, dataService, timerService) {
 				$scope.aeonusd = (data[0].price_usd * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
 				//$scope.aeonweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.aeonusd;
 			});
-	$.getJSON("https://api.coinmarketcap.com/v1/ticker/electroneum/?convert=USD", function(data) {
+			$.getJSON("https://api.coinmarketcap.com/v1/ticker/electroneum/?convert=USD", function(data) {
 				$scope.etnusd = (data[0].price_usd * 1).toFixed(3);						// CRYPTONATOR XMR/USD RATE
 				//$scope.etnweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.etnusd;
 			});
+	        	$.getJSON("https://api.coinmarketcap.com/v1/ticker/aeon/?convert=USD", function(data) {
+				$scope.grftusd = 0.0221;						// Unlisted
+				//$scope.aeonweekrev = (1000/$scope.network.difficulty)*86400*7*$scope.network.value*$scope.aeonusd;
+			});	
     timerService.register(ticker, 'poolHashrateChart');
     ticker();
 
