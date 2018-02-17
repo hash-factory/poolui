47405c4877e8ad03f498a21cd259ecff7c6a3479
Learner <linhdqtg@gmail.com>
2018-02-17 15:11:11 +0700

    Add coin var

--- app/globals.js
+++ app/globals.js
@@ -7,6 +7,8 @@ angular.module('pool.globals', [])
 		pool_name: "semiPOOL.com",
 		api_url : 'https://aeon-api.semipool.com',
 		api_refresh_interval: 10000,
-		app_update_interval: 10*60000
+		app_update_interval: 10*60000,
+		coinCode: "AEON",
+		coinName: "AEON
 	};
 });
