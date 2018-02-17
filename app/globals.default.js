e1a8e22293c7fc79673912dee6df51350324ab27
Learner <linhdqtg@gmail.com>
2018-02-17 16:46:29 +0700

    Default

--- app/globals.default.js
+++ app/globals.default.js
@@ -6,7 +6,9 @@ angular.module('pool.globals', [])
 	return {
 		pool_name: "semiPOOL.com",
 		api_url : 'https://aeon-api.semipool.com',
-		api_refresh_interval: 2000,
-		app_update_interval: 30*60000
+		api_refresh_interval: 10000,
+		app_update_interval: 10*60000,
+		coinCode: "AEON",
+		coinName: "AEON"
 	};
 });
