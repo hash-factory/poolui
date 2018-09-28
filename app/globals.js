11fe675c42b5096ae3bc21e699b46573e2ae0b8b
Interchained <president@worldvaporexpo.com>
2018-09-27 23:22:33 -0400

    Update globals.js

--- app/globals.js
+++ app/globals.js
@@ -4,15 +4,15 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "semiPOOL.com",
-		api_url : 'https://aeon-api.semipool.com',
+		pool_name: "cloud.electronero.org",
+		api_url : 'https://cloud.electronero.org',
 		api_refresh_interval: 10000,
 		app_update_interval: 10*60000,
-		coinCode: "AEON",
-		coinCode_lower: "aeon",
-		coinName: "AEON",
-		coinName_lower: "aeon",		
-		hightlight: "semiPOOL group always updates the best code improvement for efficiency even with risks. Risks give you better earnings",
-		hightlight_link: "https://semipool.com"
+		coinCode: "ETNX",
+		coinCode_lower: "etnx",
+		coinName: "ETNX",
+		coinName_lower: "etnx",		
+		hightlight: "cloudELECTRONERO group always updates the best code improvement for efficiency even with risks. Risks give you better earnings",
+		hightlight_link: "https://cloud.electronero.org"
 	};
 });
